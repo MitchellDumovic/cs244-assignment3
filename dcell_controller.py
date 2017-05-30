7721e958376ae9b646a18c0899a9f7d5e5bc17a7
Mitchell Dumovic <mitch@resonancelabs.com>
2017-05-30 11:13:50 -0700

    last changes

--- dcell_controller.py
+++ dcell_controller.py
@@ -23,6 +23,7 @@ It's roughly similar to the one Brandon Heller did for NOX.
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
+import os
 
 log = core.getLogger()
 
@@ -72,9 +73,11 @@ class DCellSwitch (object):
 
 class dcell_routing (object):
 
-  def __init__ (self, numSwitches):
+  def __init__ (self, numSwitches, n, l):
     core.openflow.addListeners(self)
     self.numSwitches = numSwitches
+    self.n = n 
+    self.l = l
     self.switchCounter = 0
 
   def _handle_ConnectionUp (self, event):
@@ -86,21 +89,85 @@ class dcell_routing (object):
       sw.connect(event.connection)
       self.switchCounter+=1
       if self.switchCounter == self.numSwitches:
-        installAllPaths()
+        self.installAllPaths()
     else:
       log.warning("Reconnecting. DPID = " + str(event.dpid) + ", switchCounter = " + self.switchCounter)
       sw.connect(event.connection)
 
-  def installAllPaths():
-    pass
+  def toList(self, s):
+    n_type = s[:2]
+    length = self.l
+    if n_type != '00':
+      length +=1
+    arr = [s[i:i + 2] for i in range(2, len(s), 2)]
+    return arr[:length]
+  def fromList(arr):
+    result = ""
+    if len(arr) == 1:
+      result += "00"
+    else:
+      result += "01"
+    for i in range(7):
+      if i < len(arr):
+        result += arr[i]
+      else:
+        result += "00"
+    return result
+
+  def installAllPaths(self):
+    paths = {}
+    for src in switches:
+      srcList = self.toList(src)
+      if len(srcList) == self.l:
+        continue
+      for dst in switches:
+        dstList = self.toList(dst)
+        if (len(dstList) == self.l):
+          continue
+        if src == dst:
+          continue
+        self.DCellRouting(toList(src), toList(dst), paths)
 
 
+
+  def DCellRouting(self, src, dest, paths):
+    if ((src, dest) in paths):
+      return paths[(src, dest)]
+    if src == dest:
+      return [switches[fromList(src)]]
+    pref = os.path.commonPrefix([src, dest])
+    m = len(pref)
+    if (m == self.l):
+      dcellpath = self.findPathInSameDCell(src, dst)
+      paths[(src, dest)] = dcellpath
+      paths[(dest, src)] = dcellpath
+      return dcellpath
+
+    (n1, n2) = self.GetLink(pref, src[m], dest[m])
+    path1 = self.DCellRouting(src, n1)
+    path2 = self.DCellRouting(n2, dest)
+
+    path = path1 + path2
+    paths[(src, dest)] = path
+    paths[(dest, src)] = path
+
+  def findPathInSameDcell(self, src, dst):
+    srcSwitch = switches[self.fromList(src)]
+    destSwitch = switches[self.fromList(dst)]
+
+    masterArr = src[:]
+    masterArr[0] = "00"
+    masterArr = masterArr[:len(src) - 1]
+
+    assert(fromList(masterArr) in switches)
+    masterSwitch = switches[self.fromList(masterArr)]
+    return [srcSwitch, masterSwitch, destSwitch]
+
+  def GetLink(self, pref, cellId1, cellId2):
+    
 
 def launch ():
   """
   Starts the component
   """
-  def start_switch (event):
-    log.debug("Controlling %s" % (event.connection,))
-    Tutorial(event.connection)
-  core.openflow.addListenerByName("ConnectionUp", start_switch)
+  core.registerNew(dcell_routing)
